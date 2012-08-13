c8bbbf0048e166d2dfd0a1b0a14f6c1f40a2f61c
Martin Gonzalez <tinchogon34@gmail.com>
2012-08-13 12:07:35 -0300

    Proyecto creado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestionVisitas::Application.config.secret_token = '8163e903b641c646f055e5bdfbd7ca8ae717bcc1f69521d1bcb6bba973709a57670a08be22178d9a0654b85cce7ef990b4ce43de765dfeb00effe8ce2243baad'
